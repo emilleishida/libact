67a023ca2e3fc8fb4b50d1b4aa0ade44039605c4
Emille Ishida <emilleishida@gmail.com>
2017-07-03 17:26:00 +0200

    include nearest neighbors

--- libact/models/__init__.py
+++ libact/models/__init__.py
@@ -5,3 +5,4 @@ from .logistic_regression import LogisticRegression
 from .perceptron import Perceptron
 from .svm import SVM
 from .sklearn_adapter import SklearnAdapter, SklearnProbaAdapter
+from .nearest_neighbors import NearestNeighbors
